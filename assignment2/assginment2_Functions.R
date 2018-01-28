47317c9a6261e3bcf9303db9288d79adeae506c5
M-Morsy <mohamed.morsey96@gmail.com>
2018-01-28 21:43:51 +0200

    Update assginment2_Functions.R

--- assignment2/assginment2_Functions.R
+++ assignment2/assginment2_Functions.R
@@ -24,15 +24,17 @@ pollutantmean <- function (directory, pollutant, id)
   # get file directory correct
   file <- c (directory , id_str, ".csv")
   file_str <- toString(file)
+  file_str <- sub(',', '/', file_str)
   file_str <- gsub(',', '', file_str)
   file_str <- gsub(' ', '', file_str)
-  # read the required data
+  # read the data into data frame
   file_data <- read.csv(file_str)
+  # get sum, number of rows with actual values & mean
   sum <- sum + (sum (file_data [[pollutant]], na.rm = TRUE ))
-  nrows <- nrows + nrow(file_data)
-  #print (nrows); print (sum); print (file_str);
+  nrows <- nrows + ( nrow(file_data) - sum (is.na(file_data) [,pollutant]) )
+  #print (nrows); #print (sum); #print (file_str);
   }
-  #print (i);  print (nrows); print (sum);
+  #print (nrows); #print (i); #print (sum);
   mean <-  sum / nrows
   mean
 }
@@ -40,7 +42,7 @@ pollutantmean <- function (directory, pollutant, id)
 complete <- function (directory, id)
 {
     # initate an empty data frame
-    complete_dataFrame <-data.frame( id = integer(),  nobs = integer())
+    complete_dataFrame <-data.frame( id = numeric (),  nobs = numeric())
     counter <- 1
     for(i in id )
     { 
@@ -57,20 +59,22 @@ complete <- function (directory, id)
       {
         # you must enter directory with / in it
         id_str <- c ("0",id_str) 
-        id_str <- toString(id_str)  
+        id_str <- toString(id_str) 
         id_str <- gsub(',', '', id_str)
         id_str <- gsub(' ', '', id_str)
       }
       # get file directory correct
       file <- c (directory , id_str, ".csv")
       file_str <- toString(file)
+      file_str <- sub(',', '/', file_str)
       file_str <- gsub(',', '', file_str)
       file_str <- gsub(' ', '', file_str)
+      # read file into data frame
       file_data <- read.csv(file_str)
-      # get nobs
-      data_file_na <- !is.na.data.frame(data_file)
+      # get nobs & concatinate to data frame
+      data_file_na <- !is.na.data.frame(file_data)
       nobs <- sum (data_file_na [,2] & data_file_na [,3])
-      complete_dataFrame [[counter,1]] <- id
+      complete_dataFrame [[counter,1]] <- i
       complete_dataFrame [[counter,2]] <- nobs
       counter <- counter +1
     }
